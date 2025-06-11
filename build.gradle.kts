9e7736401e04f1e5c267c1368ebcf5444e505a8b
4o4E <869951226@qq.com>
2025-06-11 17:33:45 +0800

    feat: 更新依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.1.21"
     `maven-publish`
     `java-library`
 }
@@ -12,26 +12,26 @@ repositories {
 }
 
 fun kotlinx(id: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$id:$version"
-fun ktor(module: String, version: String = "2.3.7") = "io.ktor:ktor-$module:$version"
+fun ktor(module: String, version: String = "2.3.13") = "io.ktor:ktor-$module:$version"
 
 dependencies {
     implementation(kotlin("reflect"))
     // coroutines
-    implementation(kotlinx("coroutines-core-jvm", "1.8.0"))
+    implementation(kotlinx("coroutines-core-jvm", "1.10.2"))
     // ktor
     implementation(ktor("client-core-jvm"))
     implementation(ktor("client-okhttp-jvm"))
     implementation(ktor("client-apache"))
     implementation(ktor("client-content-negotiation"))
     implementation(ktor("serialization-kotlinx-json"))
+    // nashorn
+    runtimeOnly("org.openjdk.nashorn:nashorn-core:15.6")
 
-    implementation("org.slf4j:slf4j-simple:2.0.12")
-    implementation("io.ktor:ktor-client-okhttp-jvm:2.3.7")
-    implementation("io.ktor:ktor-client-apache:2.3.7")
     // jsoup
     implementation("org.jsoup:jsoup:1.17.2")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.slf4j:slf4j-simple:2.0.17")
 }
 
 tasks.test {
